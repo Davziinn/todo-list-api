19d735b87a11f8ad7a92b852c0be378665a90db9
Davziinn <mdavi6085@gmail.com>
2025-09-18 08:18:19 -0300

    adicionando a camada de outbound port e adapter para minha entidade de tag

---
+++ src/main/java/br/com/menz/to_do_list_api/domain/ports/out/TagRepositoryOutboundPort.java
@@ -0,0 +1,20 @@
+package br.com.menz.to_do_list_api.domain.ports.out;
+
+import br.com.menz.to_do_list_api.domain.model.Tag;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface TagRepositoryOutboundPort {
+
+    Tag salvarTag (Tag tag);
+
+    Optional<Tag> buscarBySequencial (Tag tag);
+
+    List<Tag> buscarTodasTags ();
+
+    Tag editarTag (Tag tag);
+
+    void deletarTagBySequencial (UUID sequencial);
+}
---
+++ src/main/java/br/com/menz/to_do_list_api/infraestructure/adapter/out/TagRepositoryOutboundAdapter.java
@@ -0,0 +1,58 @@
+package br.com.menz.to_do_list_api.infraestructure.adapter.out;
+
+import br.com.menz.to_do_list_api.domain.model.Tag;
+import br.com.menz.to_do_list_api.domain.ports.out.TagRepositoryOutboundPort;
+import br.com.menz.to_do_list_api.infraestructure.entity.TagEntity;
+import br.com.menz.to_do_list_api.infraestructure.mapper.TagMapper;
+import br.com.menz.to_do_list_api.infraestructure.repository.TagSpringRepositoryJPA;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Component
+public class TagRepositoryOutboundAdapter implements TagRepositoryOutboundPort {
+
+    private final TagSpringRepositoryJPA repositoryJPA;
+    private final TagMapper mapper;
+
+    public TagRepositoryOutboundAdapter(TagSpringRepositoryJPA repositoryJPA, TagMapper mapper) {
+        this.repositoryJPA = repositoryJPA;
+        this.mapper = mapper;
+    }
+
+    @Override
+    public Tag salvarTag(Tag tag) {
+        return mapper.toModel(repositoryJPA.save(mapper.toEntity(tag)));
+    }
+
+    @Override
+    public Optional<Tag> buscarBySequencial(Tag tag) {
+        UUID sequencialExtraidoDoObjeto = tag.getSequencial();
+
+        Optional<TagEntity> tagBuscada = repositoryJPA.findById(sequencialExtraidoDoObjeto);
+
+        return tagBuscada.map(mapper::toModel);
+    }
+
+    @Override
+    public List<Tag> buscarTodasTags() {
+        List<TagEntity> listaDeTagsBuscadas = repositoryJPA.findAll();
+
+        return listaDeTagsBuscadas.stream()
+                .map(mapper::toModel)
+                .toList();
+
+    }
+
+    @Override
+    public Tag editarTag(Tag tag) {
+        return mapper.toModel(repositoryJPA.save(mapper.toEntity(tag)));
+    }
+
+    @Override
+    public void deletarTagBySequencial(UUID sequencial) {
+        repositoryJPA.deleteById(sequencial);
+    }
+}
